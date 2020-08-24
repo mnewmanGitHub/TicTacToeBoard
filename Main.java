14dc550a94477036b69c9493063ecc81966076d0
mnewmanGitHub <69568290+mnewmanGitHub@users.noreply.github.com>
2020-08-24 09:34:37 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 package sample;
+//package javafx.animation;
 import javafx.application.Application;
 //import javafx.fxml.FXMLLoader;
+import javafx.event.ActionEvent;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
@@ -11,8 +13,17 @@ import javafx.scene.shape.Rectangle;
 import static javafx.scene.paint.Color.BROWN;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.input.MouseEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+import javafx.scene.control.Button;
+import javafx.geometry.Pos;
+import javafx.event.ActionEvent;
+
+import javax.swing.*;
 
 public class Main extends Application {
     /**
@@ -21,6 +32,9 @@ public class Main extends Application {
      * Try out animations
      */
     private static int clickCount = 0;
+    private Button startOver = new Button("Restart Game");
+    private TextField player1 = new TextField("Player 1 Name");
+    private TextField player2 = new TextField("Player 2 Name");
     private Rectangle box = new Rectangle(150, 150, BROWN);
     private Rectangle box1 = new Rectangle(150, 150, BROWN);
     private Rectangle box2 = new Rectangle(150, 150, BROWN);
@@ -36,10 +50,18 @@ public class Main extends Application {
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Tic-Tac-Toe");
         //Scene scene = new Scene(root, 300, 275);
+        Label label1 = new Label("Player 1:");
+        Label label2 = new Label("Player 2:");
+        //player1 = new TextField("Player 1 Name");
+        //player2 = new TextField("Player 2 Name");
+        BorderPane bP = new BorderPane();
         GridPane ticTacToeBox = new GridPane();
+        HBox hBox = new HBox();
+        hBox.setSpacing(10);
+        //VBox vBox = new VBox();
         ticTacToeBox.setGridLinesVisible(true);
-        ticTacToeBox.setHgap(15);
-        ticTacToeBox.setVgap(15);
+        ticTacToeBox.setHgap(10);
+        ticTacToeBox.setVgap(10);
 
         /**VBox vbox = new VBox();
         VBox vbox1 = new VBox();
@@ -57,7 +79,15 @@ public class Main extends Application {
         ticTacToeBox.add(box6, 1,3);
         ticTacToeBox.add(box7, 2, 3);
         ticTacToeBox.add(box8, 3,3);
-        Handler handle = new Handler();
+        bP.setCenter(ticTacToeBox);
+        //vBox.getChildren().addAll(label1,label2);
+        RestartTheGame rS = new RestartTheGame();
+        startOver.setOnAction(rS);
+        hBox.getChildren().addAll(label1, player1, label2, player2, startOver);
+        bP.setBottom(hBox);
+        //hBox.getChildren().add(label1);
+        //ticTacToeBox.addRow(4, hBox);
+        BoxHandler handle = new BoxHandler();
         box.setOnMousePressed(handle);
         box1.setOnMousePressed(handle);
         box2.setOnMousePressed(handle);
@@ -67,51 +97,116 @@ public class Main extends Application {
         box6.setOnMousePressed(handle);
         box7.setOnMousePressed(handle);
         box8.setOnMousePressed(handle);
-        primaryStage.setScene(new Scene(ticTacToeBox, 300, 300));
+        primaryStage.setScene(new Scene(bP, 300, 300));
         primaryStage.show();
     }
 
     /**private Rectangle createRec(int width, int height, Color desiredColor) {
         return new Rectangle(width, height, BROWN);
     }*/
-        class Handler implements EventHandler<MouseEvent> {
+        class BoxHandler implements EventHandler<MouseEvent> {
             public void handle(MouseEvent e) {
-                //same color column 1
-                if((box.getFill().equals(Color.RED) || box.getFill().equals(Color.CORNFLOWERBLUE)) && box.getFill().equals(box3.getFill()) && box.getFill().equals(box6)) {
-                    System.out.println(box.getFill().toString() + " Won!");
-        //same color row 1
-                } else if((box.getFill().equals(Color.RED) || box.getFill().equals(Color.CORNFLOWERBLUE)) && box.getFill().equals(box1.getFill()) && box.getFill().equals(box2.getFill())) {
-                    System.out.println(box.getFill().toString() + " Won!");
-                } else if((box1.getFill().equals(Color.RED) || box1.getFill().equals(Color.CORNFLOWERBLUE)) && box1.getFill().equals(box4.getFill()) && box1.equals(box7.getFill())) {
-                    //same color column 2
-                    System.out.println(box1.getFill().toString() + " Won!");
-                } else if((box3.getFill().equals(Color.RED) || box3.getFill().equals(Color.CORNFLOWERBLUE)) && box3.getFill().equals(box4.getFill()) && box3.getFill().equals(box5.getFill())) {
-                    //same color row 2
-                    System.out.println(box3.getFill().toString() + " Won!");
-                } else if((box2.getFill().equals(Color.RED) || box2.getFill().equals(Color.CORNFLOWERBLUE)) && box2.getFill().equals(box5.getFill()) && box2.getFill().equals(box8.getFill())) {
-                    //same color column 3
-                    System.out.println(box2.getFill().toString() + " Won!");
-                } else if((box6.getFill().equals(Color.RED) || box6.getFill().equals(Color.CORNFLOWERBLUE)) && box6.getFill().equals(box7.getFill()) && box6.getFill().equals(box8.getFill())) {
-                    System.out.println(box6.getFill().toString() + " Won!");
+                if(clickCount == 0) {
+                    //helper method
+                    Object source = e.getSource();
+                    if (source instanceof Shape) {
+                        Shape src = (Shape) source;
+                        clickCount++;
+                        if (clickCount == 9) {
+                            System.out.print("Its a Draw!");
+                            //src.setFill(Color.RED);
+                        } else if (clickCount % 2 != 0) {
+                            src.setFill(Color.RED);
+                        } else {
+                            src.setFill(Color.CORNFLOWERBLUE);
+                        }
+                        if (isMatch()) {
+                            System.out.print("Someone Won!");
+                            return;
+                        }
+                    }
                 } else {
-                Object source = e.getSource();
-                if(source instanceof Shape) {
-                    Shape src = (Shape) source;
-                    clickCount++;
-                    if (clickCount % 2 != 0) {
-                        src.setFill(Color.RED);
+                    if(isMatch()) {
+                        return;
                     } else {
-                        src.setFill(Color.CORNFLOWERBLUE);
+                        //helper method
+                        Object source = e.getSource();
+                        if (source instanceof Shape) {
+                            Shape src = (Shape) source;
+                            clickCount++;
+                            if (clickCount == 9) {
+                                System.out.print("Its a Draw!");
+                            } else if (clickCount % 2 != 0) {
+                                src.setFill(Color.RED);
+                            } else {
+                                src.setFill(Color.CORNFLOWERBLUE);
+                            }
+                            if (isMatch()) {
+                                System.out.print("Someone Won!");
+                                return;
+                            }
+                        }
                     }
                 }
-                }
+            }
+        }
 
+        class RestartTheGame implements EventHandler<ActionEvent> {
+            public void handle(ActionEvent e) {
+                clickCount = 0;
+                box.setFill(BROWN);
+                box1.setFill(BROWN);
+                box2.setFill(BROWN);
+                box3.setFill(BROWN);
+                box4.setFill(BROWN);
+                box5.setFill(BROWN);
+                box6.setFill(BROWN);
+                box7.setFill(BROWN);
+                box8.setFill(BROWN);
+
+            }
+        }
+
+
+    private boolean isMatch() {
+            //same color column 1
+        if((box.getFill().equals(Color.RED) || box.getFill().equals(Color.CORNFLOWERBLUE)) && box.getFill().equals(box3.getFill()) && box.getFill().equals(box6)) {
+            return true;
+            //System.out.println(box.getFill().toString() + " Won!");
+            //same color row 1
+        } else if((box.getFill().equals(Color.RED) || box.getFill().equals(Color.CORNFLOWERBLUE)) && box.getFill().equals(box1.getFill()) && box.getFill().equals(box2.getFill())) {
+            return true;
+            //System.out.println(box.getFill().toString() + " Won!");
+        } else if((box1.getFill().equals(Color.RED) || box1.getFill().equals(Color.CORNFLOWERBLUE)) && box1.getFill().equals(box4.getFill()) && box1.equals(box7.getFill())) {
+            //same color column 2
+            return true;
+            //System.out.println(box1.getFill().toString() + " Won!");
+        } else if((box3.getFill().equals(Color.RED) || box3.getFill().equals(Color.CORNFLOWERBLUE)) && box3.getFill().equals(box4.getFill()) && box3.getFill().equals(box5.getFill())) {
+            //same color row 2
+            return true;
+            //System.out.println(box3.getFill().toString() + " Won!");
+        } else if((box2.getFill().equals(Color.RED) || box2.getFill().equals(Color.CORNFLOWERBLUE)) && box2.getFill().equals(box5.getFill()) && box2.getFill().equals(box8.getFill())) {
+            //same color column 3
+            return true;
+            //System.out.println(box2.getFill().toString() + " Won!");
+        } else if((box6.getFill().equals(Color.RED) || box6.getFill().equals(Color.CORNFLOWERBLUE)) && box6.getFill().equals(box7.getFill()) && box6.getFill().equals(box8.getFill())) {
+            //same color row 3
+            return true;
+            //System.out.println(box6.getFill().toString() + " Won!");
+        } else if((box.getFill().equals(Color.RED)|| box.getFill().equals(Color.CORNFLOWERBLUE)) && box.getFill().equals(box4.getFill()) && box.getFill().equals(box8.getFill())) {
+            //same color right diagnol
+            return true;
+        } else if((box2.getFill().equals(Color.RED)|| box2.getFill().equals(Color.CORNFLOWERBLUE)) && box2.getFill().equals(box4.getFill()) && box2.getFill().equals(box6.getFill())) {
+            //same color left diagnol
+            return true;
+        }  else {
+        return false;
         }
     }
-
 
 
     public static void main(String[] args) {
         launch(args);
     }
 }
+
